8cb8226daf4a9079fdae6f127437b42072403293
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-24 12:36:29 +0100

    Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import "./App.css";
+import Home from './components/Home'
+import Header from './components/ui/Header';
+import Footer from './components/ui/Footer';
 
 const App = () => {
 
   return (
-    <center>Restuarant website</center>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 };
 
