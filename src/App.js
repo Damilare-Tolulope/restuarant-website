986ecd5d5f0c5dc35941175edff39e8650e2623c
Omojiba <tolu.omojiba@gmail.com>
2022-04-25 20:30:43 +0100

    Products page done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
-
 import Contact from "./components/contact/Contact";
+import Products from "./components/products/Products";
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/products" element={<Products />} />
         </Routes>
         <Footer />
       </Router>
--- src/components/Products.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Products = () => {
-  return (
-    <div>This is the Products Page</div>
-  )
-}
-
-export default Products
